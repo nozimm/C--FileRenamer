ffbd138c51a92009a60a97ffa577e6892cf12675
Noé Zimmermann <noe.zimmi@gmail.com>
2017-10-01 21:36:44 +0200

    First Public Version

---
+++ Source/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WindowsFormsApplication2 {
+    partial class Form1 {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.dataNr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataFormat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnAddFiles = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFindReplace = new System.Windows.Forms.Button();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbNumber = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.txtSuffix = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowDrop = true;
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataNr,
+            this.dataName,
+            this.dataFormat,
+            this.dataLocation});
+            this.dataGrid.Location = new System.Drawing.Point(-1, 0);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.Size = new System.Drawing.Size(794, 247);
+            this.dataGrid.TabIndex = 1;
+            this.dataGrid.CellMouseMove += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGrid_CellMouseMove);
+            this.dataGrid.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGrid_DragDrop);
+            this.dataGrid.DragOver += new System.Windows.Forms.DragEventHandler(this.dataGrid_DragOver);
+            this.dataGrid.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGrid_MouseDown);
+            // 
+            // dataNr
+            // 
+            this.dataNr.HeaderText = "#";
+            this.dataNr.Name = "dataNr";
+            this.dataNr.ReadOnly = true;
+            this.dataNr.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataNr.Width = 50;
+            // 
+            // dataName
+            // 
+            this.dataName.HeaderText = "Filename";
+            this.dataName.Name = "dataName";
+            this.dataName.ReadOnly = true;
+            this.dataName.Width = 150;
+            // 
+            // dataFormat
+            // 
+            this.dataFormat.HeaderText = "Ext";
+            this.dataFormat.Name = "dataFormat";
+            this.dataFormat.ReadOnly = true;
+            this.dataFormat.Width = 50;
+            // 
+            // dataLocation
+            // 
+            this.dataLocation.HeaderText = "Location";
+            this.dataLocation.Name = "dataLocation";
+            this.dataLocation.ReadOnly = true;
+            this.dataLocation.Width = 500;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(694, 517);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 4;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // btnAddFiles
+            // 
+            this.btnAddFiles.Location = new System.Drawing.Point(13, 335);
+            this.btnAddFiles.Name = "btnAddFiles";
+            this.btnAddFiles.Size = new System.Drawing.Size(117, 23);
+            this.btnAddFiles.TabIndex = 8;
+            this.btnAddFiles.Text = "Add Files...";
+            this.btnAddFiles.UseVisualStyleBackColor = true;
+            this.btnAddFiles.Click += new System.EventHandler(this.btnAddFiles_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication2.Properties.Resources.drop_here_small;
+            this.pictureBox1.InitialImage = global::WindowsFormsApplication2.Properties.Resources.drop_here;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 366);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 151);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFindReplace
+            // 
+            this.btnFindReplace.Location = new System.Drawing.Point(420, 464);
+            this.btnFindReplace.Name = "btnFindReplace";
+            this.btnFindReplace.Size = new System.Drawing.Size(153, 23);
+            this.btnFindReplace.TabIndex = 14;
+            this.btnFindReplace.Text = "Replace";
+            this.btnFindReplace.UseVisualStyleBackColor = true;
+            this.btnFindReplace.Click += new System.EventHandler(this.btnFindReplace_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(473, 384);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(100, 20);
+            this.txtFind.TabIndex = 15;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(473, 421);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(100, 20);
+            this.txtReplace.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(417, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Find:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 421);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Replace:";
+            // 
+            // cbNumber
+            // 
+            this.cbNumber.AutoSize = true;
+            this.cbNumber.Location = new System.Drawing.Point(420, 341);
+            this.cbNumber.Name = "cbNumber";
+            this.cbNumber.Size = new System.Drawing.Size(87, 17);
+            this.cbNumber.TabIndex = 19;
+            this.cbNumber.Text = "Number Files";
+            this.cbNumber.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(630, 387);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Prefix";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(630, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Suffix:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(633, 464);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(136, 23);
+            this.btnAdd.TabIndex = 22;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(669, 384);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(100, 20);
+            this.txtPrefix.TabIndex = 23;
+            // 
+            // txtSuffix
+            // 
+            this.txtSuffix.Location = new System.Drawing.Point(669, 421);
+            this.txtSuffix.Name = "txtSuffix";
+            this.txtSuffix.Size = new System.Drawing.Size(100, 20);
+            this.txtSuffix.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 552);
+            this.Controls.Add(this.txtSuffix);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnFindReplace);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAddFiles);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.dataGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FRAPP- File Rename Application";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnAddFiles;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnFindReplace;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.TextBox txtSuffix;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataNr;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataFormat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataLocation;
+    }
+}
+
---
+++ Source/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication2 {
+    public partial class Form1 : Form {
+
+        public String path;
+        public String fileName = "";
+        public String extension = "";
+        List<String> paths = new List<String>();
+        List<String> newPaths = new List<String>();
+        public String Number = "";
+        private Rectangle dragBoxFromMouseDown;
+        private int rowIndexFromMouseDown;
+        private int rowIndexOfItemUnderMouseToDrop;
+
+        public Form1() {
+            InitializeComponent();
+            this.AllowDrop = true;
+
+        }
+
+
+        private void addData() {
+            dataGrid.Rows.Add(1 + paths.Count - dataGrid.Rows.Count);
+
+            for (int i = 0; i < newPaths.Count; i++) {
+                path = newPaths[i];
+                fileName = Path.GetFileNameWithoutExtension(path);
+                extension = Path.GetExtension(path);
+
+                //Calculate Nr
+                String lengthPaths = "" + newPaths.Count;
+                String length = "D" + lengthPaths.Length;
+                String zerosNr = i.ToString(length);
+
+
+                dataGrid.Rows[i].Cells[0].Value = "0" + zerosNr;
+                dataGrid.Rows[i].Cells[1].Value = fileName;
+                dataGrid.Rows[i].Cells[2].Value = extension;
+                dataGrid.Rows[i].Cells[3].Value = path;
+            }
+
+        }
+
+
+
+
+
+        private void btnRename_Click(object sender, EventArgs e) {
+            if (cbNumber.Checked == true) {
+                //Nummeriert
+                for (int i = 0; i < paths.Count; i++) {
+                    Number = Path.GetDirectoryName(newPaths[i])+ @"\" + dataGrid.Rows[i].Cells[0].FormattedValue.ToString() +"_"  
+                        + Path.GetFileNameWithoutExtension(newPaths[i]) + Path.GetExtension(newPaths[i]);
+                    try {
+                        File.Move(paths[i], Number);
+                    } catch {
+
+                    }
+                }
+
+                } else {
+                    //Not numbered
+                for (int i = 0; i < paths.Count; i++) {
+
+
+                    try {
+                        File.Move(paths[i], newPaths[i]);
+                    } catch {
+
+                    }
+                }
+            }
+        }
+
+
+        private void btnAddFiles_Click(object sender, EventArgs e) {
+            OpenFileDialog oFD1 = new OpenFileDialog();
+            oFD1.Title = "Select a File";
+            oFD1.InitialDirectory = @"Z:\files";
+            oFD1.Multiselect = true;
+
+            if (oFD1.ShowDialog() == DialogResult.OK) {
+                for (int i = 0; i < oFD1.FileNames.Length; i++) {
+                    paths.Add(oFD1.FileNames[i]);
+                    newPaths.Add(oFD1.FileNames[1]);
+                }
+                addData();
+            }
+
+        }
+
+ 
+
+        private void Form1_DragOver(object sender, DragEventArgs e) {
+            e.Effect = DragDropEffects.All;
+
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e) {
+            try {
+                String[] dragedFiles = e.Data.GetData(DataFormats.FileDrop) as string[];
+                for (int i = 0; i < dragedFiles.Length; i++) {
+                    paths.Add(dragedFiles[i]);
+                    newPaths.Add(dragedFiles[i]);
+                }
+                addData();
+            } catch (System.Exception) {
+
+            }
+        }
+
+
+        private void dataGrid_MouseDown(object sender, MouseEventArgs e) {
+
+            rowIndexFromMouseDown = dataGrid.HitTest(e.X, e.Y).RowIndex;
+
+            if (rowIndexFromMouseDown != -1) {
+          
+                Size dragSize = SystemInformation.DragSize;
+
+
+                dragBoxFromMouseDown = new Rectangle(new Point(e.X - (dragSize.Width / 2),
+                                                               e.Y - (dragSize.Height / 2)),
+                                                        dragSize);
+            } else
+                dragBoxFromMouseDown = Rectangle.Empty;
+
+        }
+
+
+
+        private void dataGrid_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e) {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left) {
+                if (dragBoxFromMouseDown != Rectangle.Empty &&
+                    !dragBoxFromMouseDown.Contains(e.X, e.Y)) {
+                  
+                    DragDropEffects dropEffect = dataGrid.DoDragDrop(
+                             dataGrid.Rows[rowIndexFromMouseDown],
+                             DragDropEffects.Move);
+                }
+            }
+
+        }
+
+        private void dataGrid_DragOver(object sender, DragEventArgs e) {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void dataGrid_DragDrop(object sender, DragEventArgs e) {
+
+            Point clientPoint = dataGrid.PointToClient(new Point(e.X, e.Y));
+
+            rowIndexOfItemUnderMouseToDrop =
+                dataGrid.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+
+            if (e.Effect == DragDropEffects.Move) {
+                DataGridViewRow rowToMove = e.Data.GetData(
+                             typeof(DataGridViewRow)) as DataGridViewRow;
+                try {
+                    dataGrid.Rows.RemoveAt(rowIndexFromMouseDown);
+                    dataGrid.Rows.Insert(rowIndexOfItemUnderMouseToDrop, rowToMove);
+                } catch (System.Exception) {
+                    try {
+                        dataGrid.Rows.Insert(rowIndexFromMouseDown, rowToMove);
+
+                    } catch (System.Exception) {
+
+                    }
+                }
+
+            }
+
+        }
+
+        private void btnFindReplace_Click(object sender, EventArgs e) {
+            for (int i = 0; i < newPaths.Count; i++) {
+                if (!txtFind.Text.Equals("") && !txtReplace.Equals("")) {
+                    String t = newPaths[i];
+                    String se = txtFind.Text;
+                    String rw = txtReplace.Text;
+                    t = Path.GetFileNameWithoutExtension(t);
+                    t = t.Replace(se, rw);
+                    newPaths[i] = Path.GetDirectoryName(newPaths[i]) + @"\" + t + Path.GetExtension(newPaths[i]);
+                    dataGrid.Rows.Clear();
+                    addData();
+                } else {
+                    if (txtFind.Text == null) {
+                    }
+                    if (txtFind == null) {
+                    }
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+            for (int i = 0; i < newPaths.Count; i++) {
+                newPaths[i] = Path.GetDirectoryName(newPaths[i]) + @"\" + txtPrefix.Text + Path.GetFileNameWithoutExtension(newPaths[i]) + txtSuffix.Text + Path.GetExtension(newPaths[i]);
+                dataGrid.Rows.Clear();
+                addData();
+            }
+        }
+    }
+}
